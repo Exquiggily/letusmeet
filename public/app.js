569aa5ac7336ff6cc8be98b0d345a5bfab917d0c
Jacob-Mc-kenzie <Jmcke262@gmail.com>
2024-01-11 17:34:18 +1000

    formatting, direction switching now behaves

--- public/app.js
+++ public/app.js
@@ -5,12 +5,12 @@ const SM = 3;
 let marginSize = 20;
 let marginPercent = 10;
 let drawflag = false;
-let timeslots = [{row:0,col0:0,col1:1}];
+let timeslots = [{ row: 0, col0: 0, col1: 1 }];
 let user = 0;
 let eventInfo = {
-  eventName:"default",
-  attendies:["Default"],
-  timeslots:[[{row:0,col0:0,col1:1, dir: 1}]],
+  eventName: "default",
+  attendies: ["Default"],
+  timeslots: [[{ row: 0, col0: 0, col1: 1, dir: 1 }]],
 };
 
 function setup() {
@@ -21,63 +21,69 @@ function setup() {
 function draw() {
   background(220);
   noStroke();
-  
+
   fill(100);
-  
-  for(let i = 0; i < eventInfo.timeslots.length; i++){
-    for(let j = 0; j< eventInfo.timeslots[i].length; j++){
-      rect((gridsize*(eventInfo.timeslots[i][j].row)+SM)+marginSize,gridsize*(eventInfo.timeslots[i][j].col0)+SM*eventInfo.timeslots[i][j].dir+marginSize,gridsize-SM*2,(gridsize*((eventInfo.timeslots[i][j].col1+1) - (eventInfo.timeslots[i][j].col0+1)))-SM*2*eventInfo.timeslots[i][j].dir,5);
+
+  for (let i = 0; i < eventInfo.timeslots.length; i++) {
+    for (let j = 0; j < eventInfo.timeslots[i].length; j++) {
+      rect((gridsize * (eventInfo.timeslots[i][j].row) + SM) + marginSize, gridsize * (eventInfo.timeslots[i][j].col0) + SM * eventInfo.timeslots[i][j].dir + marginSize, gridsize - SM * 2, (gridsize * ((eventInfo.timeslots[i][j].col1 + 1) - (eventInfo.timeslots[i][j].col0 + 1))) - SM * 2 * eventInfo.timeslots[i][j].dir, 5);
     }
   }
-fill(150);
-  circle(mouseX,mouseY,10);
-  grid(marginSize,marginSize,width-marginSize,height-marginSize);
-  if(drawflag){
+  fill(150);
+  circle(mouseX, mouseY, 10);
+  grid(marginSize, marginSize, width - marginSize, height - marginSize);
+  if (drawflag) {
     fill("red");
-    circle(20,20,5);
+    circle(20, 20, 5);
   }
 }
 
-function grid(x,y,x2,y2){
-  for(let i = 0; i< (y2-y)/gridsize; i++){
-    for(let j = 0; j < (x2-x)/gridsize;j++){
-      circle(x+(j*gridsize),y+(i*gridsize),pointSize);
+function grid(x, y, x2, y2) {
+  for (let i = 0; i < (y2 - y) / gridsize; i++) {
+    for (let j = 0; j < (x2 - x) / gridsize; j++) {
+      circle(x + (j * gridsize), y + (i * gridsize), pointSize);
     }
   }
 }
 
-function snap(x,y){
-  return [ceil(x/gridsize) , ceil(y/gridsize)];
+function snap(x, y) {
+  return [ceil(x / gridsize), ceil(y / gridsize)];
 }
-function snapUp(x,y){
-  return [ceil(x/gridsize) ,floor(y/gridsize)];
+function snapUp(x, y) {
+  return [ceil(x / gridsize), floor(y / gridsize)];
 }
 
-function mousePressed(_event){
+function mousePressed(_event) {
   drawflag = true;
-  let i = snap(mouseX-marginSize,mouseY-marginSize);
+  let i = snap(mouseX - marginSize, mouseY - marginSize);
   console.log(i);
-  if(user != -1){
-    eventInfo.timeslots[user].push( {row: i[0]-1, col0:i[1]-1, col1: i[1], dir: 1});
+  if (user != -1) {
+    eventInfo.timeslots[user].push({ row: i[0] - 1, col0: i[1] - 1, col1: i[1], dir: 1 });
   }
 }//flp the square coords when going from a down to an up
-function mouseDragged(_event){
-  let i = [marginSize,marginSize];
-  let dir = true;
-  if(eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].col0*gridsize+marginSize > mouseY &&eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].dir == 1){ //this line with else is causeing flickering
-    i = snapUp(mouseX-marginSize,mouseY-marginSize);
-    dir = false;
-  }
-  else{
-  i = snap(mouseX-marginSize,mouseY-marginSize);
-  }
-  if(user != -1 && eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].col1 != i[1]){
-    eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].col1 = i[1];
-    eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].dir = dir ? 1 : -1;
-
+function mouseDragged(_event) {
+  let i = [marginSize, marginSize];
+  if (user != -1) {
+    if (eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].col0 * gridsize + marginSize > mouseY && eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].dir == 1) {
+      eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].dir = -1;
+      eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].col0++;
+    }
+    else if (eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].col0 * gridsize + marginSize < mouseY && eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].dir == -1) {
+      eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].dir = 1;
+      eventInfo.timeslots[user][eventInfo.timeslots[user].length-1].col0--;
+    }
+    if (eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].dir == 1) {
+      i = snap(mouseX - marginSize, mouseY - marginSize);
+    }
+    else {
+      i = snapUp(mouseX - marginSize, mouseY - marginSize);
+    }
+    if (eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].col1 != i[1]) {
+      eventInfo.timeslots[user][eventInfo.timeslots[user].length - 1].col1 = i[1];
+    }
   }
 }
-function mouseReleased(_event){
+function mouseReleased(_event) {
   drawflag = false;
 }
 
@@ -86,9 +92,9 @@ function windowResized() {
   init();
 }
 
-function init(){
-  if(windowWidth < 400){
+function init() {
+  if (windowWidth < 400) {
     marginPercent = 20;
   }
-  marginSize = width > height ? width/marginPercent: height/marginPercent;
+  marginSize = width > height ? width / marginPercent : height / marginPercent;
 }
